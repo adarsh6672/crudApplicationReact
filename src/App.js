ee53a9de4274c20e18ac44fefd374c4973703119
adarsh6672 <adarshkeruvatty@gmail.com>
2024-02-04 23:26:54 +0530

    autherization

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import {Routes , Route} from 'react-router-dom'
 import Login from './Components/Login/Login';
 import SignUp from './Components/SignUp/SignUp';
+import Home from './Components/Home/Home';
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/login' element={<Login />} />
       <Route path='/signup' element={<SignUp />} />
+      <Route path='/home' element={<Home /> } />
     </Routes>
     
     </>
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,15 +1,59 @@
 import React, { useState } from 'react'
 import './Login.css'
 import { Link } from 'react-router-dom'
-
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 function Login() {
-    const [firstName , setFirstName] = useState();
-    const [lastName , setLastName] = useState();
+
     const [username , setUsername] = useState();
     const [password , setPassword] = useState();
+
+    const nav=useNavigate();
+
+    const handleClick=async ()=>{
+        const data={
+            username,
+            password
+        }
+        console.log(data)
+
+        try{
+             await axios.post("http://localhost:8080/login",data).then( async (resp)=>{
+                let token= resp.data.token
+                localStorage.setItem('token' ,token)
+                localStorage.setItem('role',resp.data.role)
+                console.log(resp)
+                if(localStorage.getItem('role')==='USER'){
+                    await axios.get("http://localhost:8080/demo",{
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                     }
+                }).then((response)=>{
+                    console.log(response)
+                    console.log('user logged in')
+                })         
+                }else{
+                    await axios.get("http://localhost:8080/admin_dashboard",{
+                    headers: {
+                        'Authorization': `Bearer ${localStorage.getItem('token')}`
+                     }
+                }).then((response)=>{
+                    console.log(response)
+                    console.log('admin loggedn in')
+                })         
+                }
+                 
+                
+            })
+          }catch(error){
+            alert(error)
+            console.log(error)
+          }
+    }
     
         
   return (
+
     <div className='container'>
     <div className="header">
         <div className="text">LOG IN</div>
@@ -32,7 +76,7 @@ function Login() {
         
     </div>
     <div className="submit-container">
-        <div className="submit">LogIn</div>
+        <div className="submit" onClick={handleClick}>LogIn</div>
         <Link to='/signup'>
         <div className="login">Go To SignUp</div>
         </Link>
